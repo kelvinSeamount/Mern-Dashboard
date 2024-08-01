be2a7e53b4eb8cd74baf615532d603c795480592
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2024-08-01 23:25:34 +0200

    add revenue charts

--- src/scenes/dashboard/Row1.tsx
+++ src/scenes/dashboard/Row1.tsx
@@ -1,10 +1,13 @@
 import {
   Area,
   AreaChart,
+  Bar,
+  BarChart,
   CartesianGrid,
   Legend,
   Line,
   LineChart,
+  Rectangle,
   ResponsiveContainer,
   Tooltip,
   XAxis,
@@ -46,6 +49,21 @@ const Row1 = () => {
           name: month.substring(0, 3),
           revenue: revenue,
           profit: (revenue - expenses).toFixed(2),
+        };
+      })
+    );
+  }, [data]);
+
+  //Create variable Revenue
+  const revenue = useMemo(() => {
+    return (
+      //when data exist
+      data &&
+      data[0]?.monthlyData?.map(({ month, revenue }) => {
+        return {
+          //grab first three letters
+          name: month.substring(0, 3),
+          revenue: revenue,
         };
       })
     );
@@ -183,7 +201,60 @@ const Row1 = () => {
           </LineChart>
         </ResponsiveContainer>
       </DashBoardBox>
-      <DashBoardBox gridArea="c"></DashBoardBox>
+      <DashBoardBox gridArea="c">
+        <BoxHeader
+          title="Revenue by Month"
+          subtitle="graph representing the revenue month by month"
+          sideText="+4%"
+        />
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            width={500}
+            height={300}
+            data={revenue}
+            margin={{
+              top: 17,
+              right: 15,
+              left: -5,
+              bottom: 58,
+            }}
+          >
+            <defs>
+              <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0.8}
+                />
+                <stop
+                  offset="95%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0}
+                />
+              </linearGradient>
+            </defs>
+            <CartesianGrid vertical={false} stroke={palette.grey[800]} />
+            <XAxis
+              dataKey="name"
+              axisLine={false}
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <YAxis
+              axisLine={false}
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <Tooltip />
+
+            <Bar
+              dataKey="revenue"
+              fill="url(#colorRevenue)"
+              activeBar={<Rectangle fill="gold" stroke="purple" />}
+            />
+          </BarChart>
+        </ResponsiveContainer>
+      </DashBoardBox>
     </>
   );
 };
